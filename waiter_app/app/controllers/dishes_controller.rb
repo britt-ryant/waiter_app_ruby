7a9f84289b8e659c9fab98c092fe5b9e2be2bb56
CRHarding <casey.r.harding@gmail.com>
2018-04-03 20:14:41 -0400

    Adding CRU for dishes

--- waiter_app/app/controllers/dishes_controller.rb
+++ waiter_app/app/controllers/dishes_controller.rb
@@ -1,22 +1,47 @@
 class DishesController < ApplicationController
   def index
+    @dishes = Dish.all
   end
 
   def show
+    @dish = Dish.find(params[:id])
   end
 
   def new
+    @dish = Dish.new
   end
 
   def create
+    @dish = Dish.new(dish_params)
+    if @dish.save
+      redirect_to dishes_path
+    else
+      redirect_to new_dish_path
+    end
   end
 
   def edit
+    @dish = Dish.find(params[:id])
   end
 
   def update
+    @dish = Dish.find(params[:id])
+    if @dish.update(dish_params)
+      redirect_to dish_path(@dish.id)
+    else
+      redirect_to edit_dish_path(@student.id)
+    end
   end
 
   def destroy
+    @dish = Dish.find(params[:id])
+    @dish.delete
+    redirect_to dishes_path
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name, :description, :course, :cost)
   end
 end
