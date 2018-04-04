bdf07369e62879100917a3b0a12b63096c2f5bd7
Ryanbritt <britt.ryant@gmail.com>
2018-04-04 16:45:42 -0400

    added allergies pages, forms to post to the user_ingredients table and a delete method to remove it

--- waiter_app/app/controllers/user_ingredients_controller.rb
+++ waiter_app/app/controllers/user_ingredients_controller.rb
@@ -3,14 +3,16 @@ class UserIngredientsController < ApplicationController
     new_params = {ingredient_id: params[:ingredient_id][0].to_i, user_id: params[:user_id][0].to_i}
     @join = UserIngredient.create(new_params)
     if @join.save
-      redirect_to ingredients_path
+      redirect_to user_path(current_user.id)
     else
-      redirect_to ingredients_path
+      redirect_to user_path(current_user.id)
     end
   end
 
   def destroy
-    p params
-    binding.pry
+    new_params = {user_id: current_user[:id], ingredient_id: params[:id]}
+    @join = UserIngredient.find_by(new_params)
+    @join.delete
+    redirect_to user_path(current_user.id)
   end
 end
