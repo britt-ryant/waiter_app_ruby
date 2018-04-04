287d87d382982a17de797ac25d5937b02b03b6b8
CRHarding <casey.r.harding@gmail.com>
2018-04-04 17:00:43 -0400

    Updating user auth

--- waiter_app/config/routes.rb
+++ waiter_app/config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
 
+  resources :users, contraints: GroupConstraint.new do
+    resources :restricted_route do
+    end 
+  end
+
   devise_for :users
   root to: "users#show"
   resources :users
   resources :dishes
   resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+class GroupConstraint
+  def initialize
+    @user_auth = current_user.auth
+  end
+
+  def matches?(request)
+    @user_auth == 1
+  end
 end
