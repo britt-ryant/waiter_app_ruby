89dd5ab0b441aa5b862c1a7755837139c5b0eceb
Ryanbritt <britt.ryant@gmail.com>
2018-04-04 17:57:18 -0400

    Added ability to add dishes to the user order, and links to the ingredientes from the dishes page and vice versa

--- waiter_app/config/routes.rb
+++ waiter_app/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :dishes
   resources :ingredients
   resources :user_ingredients
+  resources :user_dishes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
