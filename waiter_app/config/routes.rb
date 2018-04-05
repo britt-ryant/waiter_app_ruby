c5e8b8dc5295b0c97b74713797f97cf6d39f609d
Ryanbritt <britt.ryant@gmail.com>
2018-04-04 20:00:04 -0400

    I hate NJ transit

--- waiter_app/config/routes.rb
+++ waiter_app/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :dishes
   resources :ingredients
-  resources :user_ingredients
-  resources :user_dishes
+  resources :user_ingredients, only: [:create, :destroy]
+  resources :user_dishes, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
